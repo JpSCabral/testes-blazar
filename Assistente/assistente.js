bd9a18f4ae6431ab2f3cb2657b18da3b308b935d
JpSCabral <cabral.santosjp@hotmail.com>
2023-10-18 18:09:55 -0300

    Criaçao de outros componentes

---
+++ Assistente/assistente.js
@@ -0,0 +1,12 @@
+var bttExpandAssist = document.querySelector('#btt-assistente');
+var popupAssistente = document.querySelector('#popup')
+
+function expandDiv() {
+    popupAssistente.classList.toggle('expand');
+}
+
+
+// bttExpandAssist.addEventListener('click', function () {
+//     popupAssistente.classList.toggle('expand')
+    
+// })
